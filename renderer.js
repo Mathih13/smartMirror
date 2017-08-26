1a0069c3d230f39fc90e560fe58c6d2d31ba855f
Mathih13 <mathih13@uia.no>
2017-08-26 17:09:24 +0200

    init
    A very basic first setup of the smartMirror “OS”

---
+++ renderer.js
@@ -0,0 +1,8 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+// memes
+setTimeout(function(){
+  document.getElementsByClassName('funny')[0].style.display = 'block';
+}, 5000);
