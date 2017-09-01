e5703ed415656abf5cc3afb5c9accf812edb660f
Mathih13 <mathih13@uia.no>
2017-09-01 12:34:49 +0200

    changed startup sequence to allow a loading screen and initialization logic

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ const electron = require('electron')
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
+// Used to recieve messages from broweserwindows.
+const ipcMain = electron.ipcMain
 // Plugin to check if we are in "dev mode"
 const isDev = require('electron-is-dev');
+
+
+
 
 
 const path = require('path')
@@ -13,33 +18,30 @@ const url = require('url')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+let loadingWindow
 
 function createWindow () {
 
 
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, show: false, fullscreen: true})
+  loadingWindow = new BrowserWindow({width: 800, height: 600, show: false, frame: false})
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+  loadingWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'loading.html'),
     protocol: 'file:',
-    slashes: true
+    slashes: 'true'
   }))
 
-  // If the application is run in dev mode, open dev tools
-  mainWindow.on('ready-to-show', function () {
-    mainWindow.show();
-  });
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
+  loadingWindow.on('ready-to-show', () => {
+    loadingWindow.show();
   })
+
+
+
+
 }
+
+
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -62,6 +64,33 @@ app.on('activate', function () {
     createWindow()
   }
 })
+
+ipcMain.on('loaded', (event, arg) => {
+  loadingWindow.close()
+  loadingWindow = null;
+  mainWindow = new BrowserWindow({width: 800, height: 600, show: false, fullscreen: true})
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: 'true'
+  }))
+
+  mainWindow.on('ready-to-show', () => {
+
+    loadingWindow.destroy()
+    loadingWindow = null;
+    mainWindow.show();
+  })
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null
+  })
+
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
---
+++ modules/funnies/funnies.js
@@ -0,0 +1,57 @@
+import _ from 'lodash';
+import defaultMessages from './data';
+
+class Funnies {
+  constructor(messages=[], opts={}) {
+    // Should messages be appended to what is already there or be used as a substitute?
+    opts.appendMessages = typeof opts.appendMessages === 'undefined' ? true : opts.appendMessages;
+    if (opts.appendMessages) {
+      this.messages = _.shuffle(defaultMessages.concat(messages));
+    } else {
+      this.messages = _.shuffle(messages);
+    }
+
+    // convert messages into a map of message to how many times it has been
+    // used.
+    this.record = {};
+    this.messages.forEach(message => {
+      this.record[message] = 0;
+    });
+  }
+
+  // pick the smallest of the freqencies for a message to get a more random
+  // distribution
+  message() {
+    let smallestMessage = this.messages.reduce((smallest, message) => {
+      if (this.record[smallest] > this.record[message]) {
+        return message;
+      } else if (this.record[smallest] === this.record[message]) {
+        return _.sample([smallest, message]);
+      } else {
+        return smallest;
+      }
+    });
+
+    // update the recrd to show that this message was picked
+    this.record[smallestMessage] += 1;
+    return smallestMessage;
+  }
+
+  messageHTML() {
+    let message = this.message();
+    let html = `<div class="funnies">
+      <span class="loading-funny">${message}</span>
+    </div>`.replace(/(\r?\n|^ +)/gm, '');
+    return {message, html};
+  }
+}
+
+export {
+  Funnies as default,
+  Funnies
+};
+
+// for browser support
+if (typeof window !== 'undefined') {
+  window.Funnies = Funnies;
+}
